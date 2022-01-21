51a0e705e66520d9f777aa224a44daea68f7d254
vk212446 <vk212446@reigate.ac.uk>
2022-01-21 13:11:37 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your barcode (no spaces)");
+        String barcodeDigits = input.next();
+        int[] barcode = Integer.parseInt(barcodeDigits.charAt(0)+""){barcodeDigits.charAt(0), barcodeDigits.charAt(1), barcodeDigits.charAt(2), barcodeDigits.charAt(4), barcodeDigits.charAt(4), barcodeDigits.charAt(5), barcodeDigits.charAt(6), barcodeDigits.charAt(7), barcodeDigits.charAt(8), barcodeDigits.charAt(9), barcodeDigits.charAt(10), barcodeDigits.charAt(11)};
+
+        }
+    }
